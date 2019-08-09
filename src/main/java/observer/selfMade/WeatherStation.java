3fbe3de61f7d37556ef8ebbdb4903ed48381242d
Yuliia Korzhuk <cibylko@gmail.com>
2019-08-09 15:23:25 +0300

    test commit

--- src/main/java/observer/selfMade/WeatherStation.java
+++ src/main/java/observer/selfMade/WeatherStation.java
@@ -7,7 +7,7 @@ public class WeatherStation {
         CurrentConditionsDisplay currentConditionsDisplay = new CurrentConditionsDisplay(weatherData);
         StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
         ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
-        HeatIndexDisplay heatIndexDisplay = new HeatIndexDisplay(weatherData);
+        HeatIndexDisplay heatIndexDisplay = new HeatIndexDisplay(weatherData); //add comment
 
         weatherData.setMeasurements(80, 65, 30.4f);
         weatherData.setMeasurements(82, 70, 29.2f);
